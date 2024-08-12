cbc93d0f5e1310112c3c1f4d27a0783acfa43312
Sofea Emirlee <sofea@5r.com.my>
2024-08-12 17:05:18 +0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -48,7 +48,16 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-
+//<<<<<<< HEAD
+/*define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );*/
+//=======
 define( 'AUTH_KEY',         ';X[R&#1_=WH/A#ThwQ1OVq<,3[zl@>pG%V)`wKu(uM&]I1 4l<^.Pw-)Pz=F8AS)' );
 define( 'SECURE_AUTH_KEY',  't6rs[4#l,Bkf76,?6iwfADR!}T1G@.r?1-3EYO$tysZ.H9-g.&m`2)+t*3,:w(TH' );
 define( 'LOGGED_IN_KEY',    '[Drmu^hj7sSR ,fjxq]<o,`_=S,M>Za;%_V^=6yy566B%/9@MrW@dI.tL<rEb&~{' );
@@ -57,7 +66,7 @@ define( 'AUTH_SALT',        'VnmC.g5KQT,-|X@Bx+ejC}[;o[5f6lvFkcSv9m!(rH0[qk^?cc6
 define( 'SECURE_AUTH_SALT', 'se.~*tScaX8jT9-qPwd*cH<[hlg4tKKl-sBaS{q*M.]>.uA7M3*u8.r{r.O~UD9p' );
 define( 'LOGGED_IN_SALT',   'v`mV!BN|1Lv?Qd?z%:fBv&=}hmE0nF{VzY_xph|7EvJkag>OGQdPxIwdt~8f&ugP' );
 define( 'NONCE_SALT',       '.ApX^B*o)^(+A0^#Pb`Czb-*]EKuRc y3lyF~SN68Dwp(BOm=i7L_rlZ40?vnOo4' );
-
+//>>>>>>> 35a9772c7314fc1504b858431ca67853abf2c5be
 
 //#@-*/
 
